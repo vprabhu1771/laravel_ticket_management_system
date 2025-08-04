df60aa18a989ba00a5e51c38492bcafcb07a7a33
Prabhu <vprabhu1771@gmail.com>
2025-08-04 10:48:48 +0530

    AdminMenuServiceProvider added
    php artisan make:provider AdminMenuServiceProvider

---
+++ app/Providers/AdminMenuServiceProvider.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\View;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+
+class AdminMenuServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Event::listen(BuildingMenu::class, function (BuildingMenu $event) {
+
+            // Navbar items:
+            $event->menu->add(
+                [
+                    'type' => 'navbar-search',
+                    'text' => 'search',
+                    'topnav_right' => true,
+                ],
+
+                [
+                    'type' => 'fullscreen-widget',
+                    'topnav_right' => true,
+                ],
+            );
+
+            // Sidebar items:
+            $event->menu->add(
+                [
+                    'type' => 'sidebar-menu-search',
+                    'text' => 'search',
+                ],
+            );
+
+            // Sidebar items:
+            $event->menu->add(
+                [
+                    'text' => 'blog',
+                    'url' => 'admin/blog',
+                    'can' => 'manage-blog',
+                ],
+                [
+                    'text' => 'pages',
+                    'url' => 'admin/pages',
+                    'icon' => 'far fa-fw fa-file',
+                    'label' => 4,
+                    'label_color' => 'success',
+                ],
+                ['header' => 'account_settings'],
+                [
+                    'text' => 'profile',
+                    'url' => 'admin/settings',
+                    'icon' => 'fas fa-fw fa-user',
+                ],
+                [
+                    'text' => 'change_password',
+                    'url' => 'admin/settings',
+                    'icon' => 'fas fa-fw fa-lock',
+                ],
+                [
+                    'text' => 'multilevel',
+                    'icon' => 'fas fa-fw fa-share',
+                    'submenu' => [
+                        [
+                            'text' => 'level_one',
+                            'url' => '#',
+                        ],
+                        [
+                            'text' => 'level_one',
+                            'url' => '#',
+                            'submenu' => [
+                                [
+                                    'text' => 'level_two',
+                                    'url' => '#',
+                                ],
+                                [
+                                    'text' => 'level_two',
+                                    'url' => '#',
+                                    'submenu' => [
+                                        [
+                                            'text' => 'level_three',
+                                            'url' => '#',
+                                        ],
+                                        [
+                                            'text' => 'level_three',
+                                            'url' => '#',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                        [
+                            'text' => 'level_one',
+                            'url' => '#',
+                        ],
+                    ],
+                ],
+                ['header' => 'labels'],
+                [
+                    'text' => 'important',
+                    'icon_color' => 'red',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'warning',
+                    'icon_color' => 'yellow',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'information',
+                    'icon_color' => 'cyan',
+                    'url' => '#',
+                ],
+            );
+        });
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    App\Providers\AdminMenuServiceProvider::class,
     App\Providers\AppServiceProvider::class,
 ];
